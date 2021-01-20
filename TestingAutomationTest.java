671c09e11a67dc234fa3fc0915dd8166270333a3
Daniel Cierco <daniel_cierco@live.com>
2021-01-20 16:36:13 -0300

    ajustes

--- TestingAutomationTest.java
+++ TestingAutomationTest.java
@@ -4,24 +4,29 @@ import br.com.meowlenium.framework.utils.bases.BaseTest;
 import br.com.meowlenium.framework.webdrivers.DriverManager;
 import br.com.meowlenium.pageobjects.TestingAutomationPageObject;
 import com.github.javafaker.Faker;
+
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestingAutomationTest extends BaseTest
 {
-    @Test(
-            description = "Pratica de automação",
-            priority = 4
-    )
-    public void automationPractice()
-    {
-        WebDriver driver = DriverManager.getDriver();
+    private WebDriver driver;
+    private Faker faker;
+    private TestingAutomationPageObject page;
+
+
+    @Before
+    public void init(){
+        this.driver = DriverManager.getDriver();
         driver.get("http://practice.automationtesting.in/");
+        this.faker = new Faker();
+        this.page = new TestingAutomationPageObject(driver);
+    }
 
-        Faker faker = new Faker();
-      
-
-
+    @Test(description = "Teste de Registrar Usuário",priority = 4)
+    public void registerTest()
+    {    
     }
 }
